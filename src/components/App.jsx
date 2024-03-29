26589563982df320c35c061189c671a1daac4dc7
Andree-Ioana <ioana.vinereanu@stud.etti.upb.ro>
2024-03-29 12:11:07 +0200

    create my profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
-import Profile from './Profile/Profile';
+
 import user from '../data/user.json';
+import Profile from './Profile/Profile';
 
 
 export const App = () => {
- 
-
   return (
     <>
-
       <Profile
         username={user.username}
         tag={user.tag}
